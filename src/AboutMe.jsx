7769e912c81b6113a5b6099dde0244f1b048a275
jalonba-x <javier.barrera@ug.uchile.cl>
2026-07-12 19:10:06 -0400

    Update AboutMe.jsx

--- src/AboutMe.jsx
+++ src/AboutMe.jsx
@@ -16,7 +16,7 @@ const MAIN_IMAGES = [mainm, mainm2, mainf];
 
 const REVEAL_CONTENT = [
   {
-    upper: ["Hi!, I'm a freelance EN-ES  language localization specialist",
+    upper: ["Hi!, I'm a freelance EN-ES language localization specialist",
            "Aiming   to   jump   into   the   gaming   industry",]
     lower: "Name: Javier Barrera", "Age: 33", "Location: Santiago, Chile",
   },
