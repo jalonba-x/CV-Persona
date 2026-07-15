9e375cd56025d6a7f40f74185be113882d81a191
jalonba-x <javier.barrera@ug.uchile.cl>
2026-07-15 17:40:23 -0400

    Update VideoPage.jsx

--- src/VideoPage.jsx
+++ src/VideoPage.jsx
@@ -4,6 +4,17 @@ import { useNavigate } from 'react-router-dom'
 export default function VideoPage({ src }) {
   const navigate = useNavigate()
 
+  const videoContainerStyle = {
+  position: "fixed",
+  top: 0,
+  left: 0,
+  width: "100vw",
+  height: "100vh",
+  objectFit: "cover",
+  zIndex: -1,              // MUST BE NEGATIVE ONE to push it behind text/buttons!
+  pointerEvents: "none",   // ALLOWS clicks to pass right through the video to the buttons
+};
+
   useEffect(() => {
     const onKey = (e) => {
       if (e.key === 'ArrowLeft') navigate(-1)
