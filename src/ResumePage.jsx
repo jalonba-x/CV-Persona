8a0e510ef312c666f85c8e5b098ed732b6c92631
jalonba-x <javier.barrera@ug.uchile.cl>
2026-07-22 18:49:47 -0400

    Update ResumePage.jsx

--- src/ResumePage.jsx
+++ src/ResumePage.jsx
@@ -491,7 +491,7 @@ export default function ResumePage() {
 
         .sc-footer {
   position: absolute;
-  bottom: 2.6cqh; right: 7.0cqw;
+  bottom: 2.6cqh; right: 7.2cqw;
   z-index: 20;
   display: flex; flex-direction: column;
   align-items: flex-end; gap: 0.5cqh;
